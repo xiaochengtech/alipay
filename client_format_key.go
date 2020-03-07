19e155d53090f06ce697a16d10feee24ab4b7a73
Xiaosong Gao <gxsshallot@163.com>
2020-03-08 02:05:51 +0800

    Fix：多余的import。

--- client_format_key.go
+++ client_format_key.go
@@ -7,11 +7,11 @@ func (c *Client) FormatPrivateKey(privateKey string) string {
 	buffer := new(bytes.Buffer)
 	buffer.WriteString("-----BEGIN RSA PRIVATE KEY-----\n")
 	rawLen, keyLen := 64, len(privateKey)
-	raws, temp := keyLen / rawLen, keyLen % rawLen
+	raws, temp := keyLen/rawLen, keyLen%rawLen
 	if temp > 0 {
 		raws++
 	}
-	start, end := 0, 0 + rawLen
+	start, end := 0, 0+rawLen
 	for i := 0; i < raws; i++ {
 		if i == raws-1 {
 			buffer.WriteString(privateKey[start:])
@@ -31,11 +31,11 @@ func (c *Client) FormatPublicKey(publicKey string) string {
 	buffer := new(bytes.Buffer)
 	buffer.WriteString("-----BEGIN PUBLIC KEY-----\n")
 	rawLen, keyLen := 64, len(publicKey)
-	raws, temp := keyLen / rawLen, keyLen % rawLen
+	raws, temp := keyLen/rawLen, keyLen%rawLen
 	if temp > 0 {
 		raws++
 	}
-	start, end := 0, 0 + rawLen
+	start, end := 0, 0+rawLen
 	for i := 0; i < raws; i++ {
 		if i == raws-1 {
 			buffer.WriteString(publicKey[start:])
--- client_sign.go
+++ client_sign.go
@@ -14,7 +14,6 @@ import (
 	"errors"
 	"fmt"
 	"hash"
-	"net/url"
 	"sort"
 )
 
